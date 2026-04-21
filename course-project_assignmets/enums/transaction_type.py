d934c17c9d6aeaa8b6ccb08fc2bd8615c703b403
Daria Kibenko <113472902+Daria-Kibenko@users.noreply.github.com>
2026-04-21 06:29:52 +0300

    Delete course-project_assignmets/enums directory

--- course-project_assignmets/enums/transaction_type.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class TransactionType(str, Enum):
-    DEPOSIT = "deposit"  # пополнение баланса пользователем
-    DEBIT = "debit"  # списание за предсказание
-    ADMIN_DEPOSIT = "admin_deposit"  # пополнение администратором
